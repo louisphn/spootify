ebfb532cc4062d5903cb43cef6771c3b52b24062
Louis Phan <louisphan@adminnoMacBook-Air-5.local>
2021-10-21 12:30:41 +0900

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,29 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+    safelist: [
+      'gray-300',
+      'red-300',
+      'yellow-300',
+      'green-300',
+      'blue-300',
+      'indigo-300',
+      'purple-300',
+      'pink-300',
+      'gray-300',
+      'red-300',
+      'yellow-300',
+      'green-300',
+      'blue-300',
+      'indigo-300',
+      'purple-300',
+      'pink-300',
+      'red-300',
+      'yellow-300',
+      'green-300',
+      'lg:absolute',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
